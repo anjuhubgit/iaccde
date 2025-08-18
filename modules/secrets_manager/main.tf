55e8a65205cd601962530b388f94d9462dddb567
anjuhubgit <anjumhiremath196@gmail.com>
2025-08-18 22:37:52 +0530

    Create main.tf

---
+++ modules/secrets_manager/main.tf
@@ -0,0 +1,13 @@
+resource "aws_secretsmanager_secret" "db_password" {
+  name = "${var.env}/db/password"
+  tags = {
+    Environment = var.env
+  }
+  
+}
+
+resource "aws_secretsmanager_secret_version" "db_password_version" {
+  secret_id = aws_secretsmanager_secret.db_password.id
+  secret_string = var.db_password
+  
+}
