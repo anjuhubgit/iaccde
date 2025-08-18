4c9986b583723b562e153a28878f4c722b64b9a7
anjuhubgit <anjumhiremath196@gmail.com>
2025-08-18 22:34:09 +0530

    Add files via upload

---
+++ modules/ecr/variables.tf
@@ -0,0 +1,9 @@
+variable "env" {
+  description = "Name of the environment."
+  type        = string
+}
+
+variable "repo_name" {
+  description = "Name of the ECR repository."
+  type        = string
+}
