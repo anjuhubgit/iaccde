28f90ee7ceb9df0a3592cc09d43b4fe8cf0814ca
anjuhubgit <anjumhiremath196@gmail.com>
2025-09-27 15:47:21 +0530

    Update main.tf

--- environment/prod/main.tf
+++ environment/prod/main.tf
@@ -1,1 +1,47 @@
+module "vpc" {
+  source = "C:/Users/souls/OneDrive/Documents/iac/modules/vpc"
+  env = var.env
+  vpc_cidr = "10.0.0.0/16"
+  public_subnets_cidr = ["10.0.1.0/24", "10.0.2.0/24"]
+  private_subnets_cidr = ["10.0.101.0/24", "10.0.102.0/24"]
+  
+  
+}
 
+module "ec2" {
+  source = "C:/Users/souls/OneDrive/Documents/iac/modules/ec2"
+  ami_id = "ami-0c55b159cbfafe1f0"
+  instance_type = "t2.micro"
+  keypair_name = "devkey"
+  env = var.env
+  subnet_id = module.vpc.public_subnet_ids[0]
+  #vpc_id = module.vpc.vpc_id
+}
+
+module "secrets_manager" {
+  source = "C:/Users/souls/OneDrive/Documents/iac/modules/secrets_manager"
+  env = var.env
+  db_password = var.db_password
+}
+
+module "rds" {
+  source = "C:/Users/souls/OneDrive/Documents/iac/modules/rds"
+  env = var.env
+  vpc_id                = module.vpc.vpc_id
+  subnet_ids            = module.vpc.private_subnet_ids
+  instance_class        = "db.t3.micro"
+  db_password           = module.secrets_manager.db_password
+  app_security_group_id = module.ec2.ec2-sg-id
+  
+}
+module "s3_assets" {
+  source       = "C:/Users/souls/OneDrive/Documents/iac/modules/s3"
+  env = var.env
+  bucket_name  = "dev-app-assets"
+}
+
+module "ecr_repo" {
+  source  = "C:/Users/souls/OneDrive/Documents/iac/modules/ecr"
+  env     = var.env
+  repo_name  = "dev-web-app"
+}
